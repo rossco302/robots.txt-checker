6d6ee988b2e282b5b62c1e448e1adaf9155d2a61
rossco302 <ross.hull302@gmail.com>
2021-05-24 16:52:04 +0100

    added text output instead of bool, added prints to create new lines in output

--- robots.py
+++ robots.py
@@ -2,6 +2,7 @@ def robots_reader(url):
 	import urllib.robotparser
 	import re
 
+	print()
 	robots_url = re.findall('^https.+?org/', url)
 	if len(robots_url) > 0:
 		robots_url = robots_url[0] + 'robots.txt'
@@ -9,12 +10,17 @@ def robots_reader(url):
 	if len(robots_url) > 0:
 		robots_url = robots_url[0] + 'robots.txt'
 
-	print(robots_url)
+	print('robots url: ' + robots_url)
+	print()
 
 	rp = urllib.robotparser.RobotFileParser()
 	rp.set_url(robots_url)
 	rp.read()
 	
-	return print(rp.can_fetch('*', url))
+	if rp.can_fetch('*', url) == True:
+		can_scrape = 'You can scrape this website'
+	elif: rp.can_fetch('*', url) == False:
+		can_scrape = 'You cannot scrape this website'
+	return can_scrape
 
 robots_reader(input('check url: '))
