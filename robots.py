cc118f3d109022cddf4d37a1c80e9fa45d9caf6a
rossco302 <ross.hull302@gmail.com>
2021-05-24 16:35:54 +0100

    changed funtion so only takes question url, get robots.txt url iwth regex

--- robots.py
+++ robots.py
@@ -1,5 +1,9 @@
-def robots_reader(url, robots_url):
+def robots_reader(url):
 	import urllib.robotparser
+	import re
+
+	robots_url = re.findall('^https.+?org', url)
+	robots_url = robots_url[0]
 
 	rp = urllib.robotparser.RobotFileParser()
 	rp.set_url(robots_url)
@@ -7,5 +11,4 @@ def robots_reader(url, robots_url):
 	
 	return print(rp.can_fetch('*', url))
 
-robots_reader('https://docs.python.org/3/library/urllib.robotparser.html', 'https://docs.python.org/robots.txt')
-
+robots_reader('https://docs.python.org/3/library/urllib.robotparser.html')
