26708f01e015d58a9e32f2027434388eaf349848
rossco302 <ross.hull302@gmail.com>
2021-05-24 16:44:16 +0100

    added .com capability to function

--- robots.py
+++ robots.py
@@ -2,8 +2,14 @@ def robots_reader(url):
 	import urllib.robotparser
 	import re
 
-	robots_url = re.findall('^https.+?org', url)
-	robots_url = robots_url[0]
+	robots_url = re.findall('^https.+?org/', url)
+	if len(robots_url) > 0:
+		robots_url = robots_url[0] + 'robots.txt'
+	robots_url = re.findall('^https.+?com/', url)
+	if len(robots_url) > 0:
+		robots_url = robots_url[0] + 'robots.txt'
+
+	print(robots_url)
 
 	rp = urllib.robotparser.RobotFileParser()
 	rp.set_url(robots_url)
@@ -11,4 +17,4 @@ def robots_reader(url):
 	
 	return print(rp.can_fetch('*', url))
 
-robots_reader('https://docs.python.org/3/library/urllib.robotparser.html')
+robots_reader('https://www.reddit.com/r/learnpython/comments/n8l53r/robotstxt_useragent_disallow_admin/')
