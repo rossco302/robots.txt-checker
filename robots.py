b45b7e09612cc8f0616805cb02e7947933e1ee06
rossco302 <ross.hull302@gmail.com>
2021-05-24 17:09:09 +0100

    working version

--- robots.py
+++ robots.py
@@ -1,26 +1,31 @@
 def robots_reader(url):
 	import urllib.robotparser
 	import re
-
+	global robots_url
+	
 	print()
 
 	if len(re.findall('^https.+?org/', url)) > 0:
-		robots_url = robots_url[0] + 'robots.txt'
+		robots_url = re.findall('^https.+?org/', url)
+		robs_url = robots_url[0] + 'robots.txt'
 	elif len(re.findall('^https.+?com/', url)) > 0:
-		robots_url = robots_url[0] + 'robots.txt'
+		robots_url = re.findall('^https.+?com/', url)
+		robs_url = robots_url[0] + 'robots.txt'
+	else:
+		return False
 
-
-	print('robots url: ' + robots_url)
+	print('robots url: ' + robs_url)
 	print()
 
 	rp = urllib.robotparser.RobotFileParser()
-	rp.set_url(robots_url)
+	rp.set_url(robs_url)
 	rp.read()
 	
 	if rp.can_fetch('*', url) == True:
 		can_scrape = 'You can scrape this website'
 	elif rp.can_fetch('*', url) == False:
 		can_scrape = 'You cannot scrape this website'
-	return can_scrape
+	
+	return print(can_scrape+'\n')
 
-robots_reader(input('check url: '))
+robots_reader('https://docs.python.org/3/library/urllib.robotparser.html')
